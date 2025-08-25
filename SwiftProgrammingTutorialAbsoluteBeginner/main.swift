abefdbc3aed870137770819f9f5d7c874eb8067f
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-08-24 23:24:06 -0700

    commit 1

--- SwiftProgrammingTutorialAbsoluteBeginner/main.swift
+++ SwiftProgrammingTutorialAbsoluteBeginner/main.swift
@@ -1,3 +1,51 @@
 // types
 
-var hightScores : Int = 0
+// ---------------------------- let var ---------------
+
+//var hightScores : Int = 0
+//hightScores = 55
+//
+//let myName = "Maksim"
+//
+//var currentActiveUsers = 124
+//currentActiveUsers = 1458
+
+
+
+// -------------------------- Array ------------------
+
+
+//var ages : [Int] = [1, 6,33, 25, 98, 75, 81, 29, 90]
+//print(ages.count)
+//print(ages.first!)
+//print(ages.last!)
+//
+//// добавление элемента в конец
+//ages.append(100)
+//print("ages:", ages)
+//
+//// добавление в определённую ячейку массива по выбору
+//ages.insert(122, at: 0)
+//print("ages:", ages)
+//
+//// сортировка масссива
+//ages.sort()
+//print("ages - sort:", ages)
+//
+//// обратная сортировка
+//ages.reverse( )
+//print("ages - reverse:", ages)
+//
+//// перемешивание массива
+//ages.shuffle()
+//print("ages - shuffle:", ages)
+
+
+// --------------------------------- Set ------------------------------------------------
+
+var ages : Set = Set([1, 6, 33, 25, 98, 75, 81, 29, 90, 1, 6])
+print(ages)
+print(ages.count)
+
+
+// 53:00
