50140a4ce06f188e3763478ab41e8b05c2ded39a
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-09-03 14:03:49 -0700

    learn swift 1:20

--- SwiftProgrammingTutorialAbsoluteBeginner/main.swift
+++ SwiftProgrammingTutorialAbsoluteBeginner/main.swift
@@ -43,9 +43,72 @@
 
 // --------------------------------- Set ------------------------------------------------
 
-var ages : Set = Set([1, 6, 33, 25, 98, 75, 81, 29, 90, 1, 6])
-print(ages)
-print(ages.count)
+//var ages : Set = Set([1, 6, 33, 25, 98, 75, 81, 29, 90, 1, 6])
+//
+//var agesSet = Set(ages)
+//print(agesSet)
+//
+//
+//print(agesSet.contains(6))
+//
+//agesSet.insert(1000)
+//print(agesSet)
 
 
-// 53:00
+// -------------------------------- Dictionary ------------------------------------------
+
+
+//let devices: [String : String] = [
+//    "phone": "iPhone SE 22",
+//    "laptop" : "MacBook Air M1",
+//    "tablet" : "iPad 10.2",
+//    "desctop" : "Asus FX 507UD"
+//]
+//
+//print(devices["phone"]!)
+//print(devices["desctop"]!)
+
+
+// -------------------------------- Function ------------------------------------------
+  
+
+//func printInstructorsName(name:String){
+//    print("Print developer \(name)")
+//}
+//
+//
+//printInstructorsName(name: "Maksim Minakov")
+//
+//
+//func add (firstNumber: Int, giraffe secondNumber: Int) -> Int{
+//    let sum = firstNumber + secondNumber
+//    return sum
+//}
+//
+//print(add(firstNumber: 20, giraffe: 20))
+//print(add(firstNumber: 50, giraffe: 50))
+
+
+
+// -------------------------------- if else ------------------------------------------
+
+
+//var isDarkModeOn = false
+//
+//if isDarkModeOn  {
+//    print("OK")
+//} else {
+//    print("NO")
+//}
+//
+//
+//var myHighScores = 555
+//var yourHighScore = 444
+//
+//if myHighScores > yourHighScore {
+//    print("Yes")
+//} else {
+//    print("NO - NO")
+//}
+
+// 1 : 20
